21b4f5e97a9120446c067b293288ee7e293297d2
Asfaakhan <khanzadiasfi0314@gmail.com>
2024-03-15 07:49:49 -0700

    first commit

---
+++ variable_cases.js
@@ -0,0 +1,20 @@
+"use strict";
+//camelCase (1st letter small and 2nd capital )
+let countryName = 'Pakistan';
+let birthMonth = 11;
+let userNameExist = true;
+let anyType; //type = any, value = undefined
+//snake_case (1st and 2nd small but words k center me underscore "_" aye ga )
+let country_name = 'Australia';
+let birth_month = 11;
+let user_name_exist = false;
+let any_type; //any 
+//PascalCase (1st and 2nd word k dono letter capital without space )
+let CountryName = 'Saudia';
+let BirthMonth = 11;
+let UserNameExist = true;
+let AnyType;
+console.log(countryName, country_name, CountryName);
+console.log(birthMonth, birth_month, BirthMonth);
+console.log(userNameExist, user_name_exist, UserNameExist);
+console.log(anyType, any_type, AnyType);
---
+++ variable_cases.ts
@@ -0,0 +1,22 @@
+//camelCase (1st letter small and 2nd capital )
+let countryName :string = 'Pakistan';
+let birthMonth :number = 11;
+let userNameExist :boolean = true 
+let anyType; //type = any, value = undefined
+
+//snake_case (1st and 2nd small but used "_" underscore in the middle of the words )
+let country_name :string = 'Australia';
+let birth_month :number = 11;
+let user_name_exist :boolean = false 
+let any_type; //any 
+
+//PascalCase (1st and 2nd letters capital without space   )
+let CountryName :string = 'Saudia';
+let BirthMonth :number = 11;
+let UserNameExist :boolean = true 
+let AnyType;
+
+console.log(countryName,country_name,CountryName);
+console.log(birthMonth,birth_month,BirthMonth);
+console.log(userNameExist, user_name_exist,UserNameExist);
+console.log(anyType,any_type,AnyType);
